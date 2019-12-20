4918da8ff5da470d6dc785ac03e857c5488e2d92
十木 <jukisan0525@gmail.com>
2019-12-20 15:30:53 +0800

    CRT显示效果

---
+++ Shader_Library/Assets/Scripts/CRT/CRTImageEffect.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class CRTImageEffect : MonoBehaviour 
+{
+	public Shader crtShader;
+	public Texture2D basePixel;
+	[Range(1, 100)]
+	public int pixelNum = 10;
+
+	public Material CRTMaterial
+	{
+		get
+		{
+			return CheckShaderAndMaterial();
+		}
+	}
+	private Material m_material = null;
+
+	private void OnRenderImage(RenderTexture screen, RenderTexture result)
+	{
+		if (CRTMaterial != null)
+		{
+			m_material.SetTexture("_PixelTex", basePixel);
+			m_material.SetInt("_PixelNum", pixelNum);
+			Graphics.Blit(screen, result, m_material);
+		}
+		else
+		{
+			Graphics.Blit(screen, result);
+		}
+	}
+
+	private Material CheckShaderAndMaterial()
+	{
+		if (crtShader == null)
+		{
+			return null;
+		}
+		if (crtShader.isSupported && m_material != null && m_material.shader == crtShader)
+		{
+			return m_material;
+		}
+		if (!crtShader.isSupported)
+		{
+			return null;
+		}
+		else
+		{
+			m_material = new Material(crtShader);
+			m_material.hideFlags = HideFlags.DontSave;
+			if (m_material)
+			{
+				return m_material;
+			}
+			else
+			{
+				return null;
+			}
+		}
+	}
+
+}
---
+++ Shader_Library/Assets/Shaders/CRT/CRTshader.shader
@@ -0,0 +1,62 @@
+﻿Shader "Unlit/CRTshader"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_PixelTex ("Pixel Texture", 2D) = "white" {}
+		_PixelNum ("Pixel Num", Int) = 1
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			sampler2D _PixelTex;
+			int _PixelNum;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				UNITY_TRANSFER_FOG(o,o.vertex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 color = tex2D(_MainTex, i.uv);
+				UNITY_APPLY_FOG(i.fogCoord, col);
+				fixed2 pixel_uv = frac(i.uv * _PixelNum);
+				fixed3 pixelColor = tex2D(_PixelTex, pixel_uv).rgb;
+				return color * fixed4(pixelColor, 1.0);
+			}
+			ENDCG
+		}
+	}
+}
